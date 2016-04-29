e6de98a16f78d4e095c438c0978884f855cdfcfb
frank184 <francoisbelanger1993@hotmail.com>
2016-04-28 23:05:43 -0400

    chapter 6

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -1,5 +1,5 @@
-module Test
-  class Output
+module Fake
+  class STDOUT
     def messages
       @messages ||= Array.new
     end
@@ -11,18 +11,33 @@ module Test
 end
 
 def out
-  @out ||= Test::Output.new
+  @out ||= Fake::STDOUT.new
 end
 
+# features/codebreaker_starts_game.feature
 Given(/^I am not yet playing$/) do
   # nothing
 end
 
 When(/^I start a new game$/) do
   @game = Codebreaker::Game.new(out)
-  @game.start
+  @game.start('1234')
 end
 
 Then(/^I should see "([^"]*)"$/) do |message|
   expect(out.messages).to include(message)
+end
+
+# features/codebreaker_submits_guess.feature
+Given(/^the secret code is "([^"]*)"$/) do |code|
+  @game = Codebreaker::Game.new(out)
+  @game.start(code)
+end
+
+When(/^I guess "([^"]*)"$/) do |guess|
+  @game.guess(guess)
+end
+
+Then(/^the mark should be "([^"]*)"$/) do |mark|
+  expect(@out.messages).to include(mark)
 end
