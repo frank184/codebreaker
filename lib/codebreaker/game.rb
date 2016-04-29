a61fbee98b91bef8a92f24902525f1bc7887948a
frank184 <francoisbelanger1993@hotmail.com>
2016-04-29 00:12:29 -0400

    chapter 7

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -5,11 +5,24 @@ module Codebreaker
     end
 
     def start(secret)
+      @secret = secret
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
     end
 
     def guess(guess)
+      mark = ''
+      4.times { |i| mark << '+' if exact_match?(guess, i) }
+      4.times { |i| mark << '-' if number_match?(guess, i) }
+      @output.puts(mark)
+    end
+
+    def exact_match?(guess, index)
+      @secret[index].eql?(guess[index])
+    end
+
+    def number_match?(guess, index)
+      @secret.include?(guess[index]) && !exact_match?(guess, index)
     end
   end
 end
