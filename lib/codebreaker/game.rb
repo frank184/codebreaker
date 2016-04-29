19c0ae3007c7ea8ecaf1d593eb341cc6bd151d0f
frank184 <francoisbelanger1993@hotmail.com>
2016-04-29 10:14:54 -0400

    chapter 8

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -12,27 +12,9 @@ module Codebreaker
     end
 
     def guess(guess)
-      @output.puts('+' * count_exact_matches(guess) + '-' * count_number_matches(guess))
+      marker = Marker.new(@secret, guess)
+      @output.puts('+' * marker.count_exact_matches + '-' * marker.count_number_matches)
     end
 
-    def exact_match?(guess, index)
-      @secret[index].eql?(guess[index])
-    end
-
-    def number_match?(guess, index)
-      @secret.include?(guess[index]) && !exact_match?(guess, index)
-    end
-
-    def count_exact_matches(guess)
-      4.times.inject(0) do |count, index|
-        count + (exact_match?(guess, index) ? 1 : 0)
-      end
-    end
-
-    def count_number_matches(guess)
-      4.times.inject(0) do |count, index|
-        count + (number_match?(guess, index) ? 1 : 0)
-      end
-    end
   end
 end
---
+++ spec/codebreaker/marker_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+require 'codebreaker_helper'
+
+module Codebreaker
+  describe Marker do
+    describe "#count_exact_matches" do
+      context "with no matches" do
+        it "returns 0" do
+          marker = Marker.new('1234', '5555')
+          expect(marker.count_exact_matches).to be(0)
+        end
+      end
+      context "with 1 exact match" do
+        it "returns 1" do
+          marker = Marker.new('1234', '1555')
+          expect(marker.count_exact_matches).to be(1)
+        end
+      end
+      context "with 1 number match" do
+        it "returns 0" do
+          marker = Marker.new('1234', '2555')
+          expect(marker.count_exact_matches).to be(0)
+        end
+      end
+      context "with 1 exact match and 1 number match" do
+        it "returns 1" do
+          marker = Marker.new('1234', '1525')
+          expect(marker.count_exact_matches).to be(1)
+        end
+      end
+    end
+    describe "#count_number_matches" do
+      context "with no matches" do
+        it "returns 0" do
+          marker = Marker.new('1234', '5555')
+          expect(marker.count_number_matches).to be(0)
+        end
+      end
+      context "with 1 number match" do
+        it "returns 1" do
+          marker = Marker.new('1234', '2555')
+          expect(marker.count_number_matches).to be(1)
+        end
+      end
+      context "with 1 exact match" do
+        it "returns 0" do
+          marker = Marker.new('1234', '1555')
+          expect(marker.count_number_matches).to be(0)
+        end
+      end
+      context "with 1 exact match and 1 number match" do
+        it "returns 1" do
+          marker = Marker.new('1234', '1525')
+          expect(marker.count_number_matches).to be(1)
+        end
+      end
+    end
+  end
+end
